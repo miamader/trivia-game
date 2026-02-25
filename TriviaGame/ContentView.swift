a5bb9b23d720325d3367e11884d75fdb7c22458e
Mia Mader <mmade014@fiu.edu>
2026-02-25 16:21:16 -0500

    Initial Commit

---
+++ TriviaGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TriviaGame
+//
+//  Created by Mia Mader on 2/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Mia Mader on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
